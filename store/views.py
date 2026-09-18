9db99623b4abb4eacae074c8114452965b5679e4
eldudyaguirre <143123379+eldudyaguirre@users.noreply.github.com>
2026-09-18 17:49:59 -0500

    Limpiar vistas de ecommerce y registro

--- store/views.py
+++ store/views.py
@@ -1,39 +1,35 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .forms import SignUpForm
-from django.contrib.auth.models import User
-from django.contrib.auth.hashers import make_password
-from datetime import datetime
-from .cart_session import ShoppingCartSession
-import json
-from django.http import JsonResponse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import redirect, render
+
 
 def error_404(request, exception):
-    return render(request, '404.html',{})
+    return render(request, '404.html')
 
 
 def home(request):
     return render(request, 'index.html')
 
+
 def do_signin(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+
+    form = AuthenticationForm(request, data=request.POST or None)
+
     if request.method == 'POST':
-        form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
-            
-            user = authenticate(username=username, password=password)
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+
             if user is not None:
                 login(request, user)
                 return redirect('home')
-            else:
-                messages.error(request, 'Usuario o contraseña inválidos.')
-        else:
-            messages.error(request, 'Usuario o contraseña inválidos.')        
-    
-    form = AuthenticationForm()
+
+        messages.error(request, 'Usuario o contraseña inválidos.')
+
     return render(request, 'sign-in.html', {'signin_form': form})
 
 
@@ -42,44 +38,5 @@ def do_logout(request):
     return redirect('home')
 
 
-def do_signup(request):
-    if request.method == "POST":        
-        sign_up_form = SignUpForm(request.POST)
-        if sign_up_form.is_valid():
-            username = sign_up_form.cleaned_data.get('username')
-            password = sign_up_form.cleaned_data.get('password1')
-            nombre = sign_up_form.cleaned_data.get('nombre')
-            apellido = sign_up_form.cleaned_data.get('apellido')
-            email = sign_up_form.cleaned_data.get('email')
-            
-            if User.objects.filter(username=username).exists():
-                messages.error(request, "El username ingresado ya está siendo utilizado!")
-            else:            
-                new_user = User(
-                    username=username,
-                    password=make_password(password),
-                    is_superuser=False,
-                    first_name=nombre,
-                    last_name=apellido,
-                    email=email,
-                    is_staff=False,
-                    is_active=True,
-                    date_joined=datetime.now()
-                )
-                new_user.save()
-                                
-                return redirect('success_signup')
-    else:
-        sign_up_form = SignUpForm()        
-    
-    return render(request, 'sign-up.html', {'sign_up_form': sign_up_form})
-            
-
-
-def success_signup(request):
-    return render(request, 'success_signup.html')
-
-
 def about(request):
     return render(request, 'about.html')
-
