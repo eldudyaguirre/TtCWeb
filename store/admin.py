76647c93e754983e0827c4e28e7c261a0367ff13
eldudyaguirre <143123379+eldudyaguirre@users.noreply.github.com>
2026-09-18 12:12:10 -0500

    Remove order administration

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from store.models import Categoria, Producto, Pedido, ItemPedido
+from store.models import Categoria, Producto
 
 admin.site.site_header = 'Administrador de TotalCountsWeb'
 
@@ -10,20 +10,3 @@ class CategoriaAdmin(admin.ModelAdmin):
 @admin.register(Producto)
 class ProductoAdmin(admin.ModelAdmin):
     list_display = ('id', 'nombre', 'descripcion', 'precio', 'activo', 'fecha_registro', 'fecha_ult_act')
-
-class ItemPedidoTabInline(admin.TabularInline):
-    list_display = ('id', 'producto', 'cantidad')
-    model = ItemPedido
-    extra = 0
-    
-    @admin.display(description='Subtotal')
-    def get_subtotal(self, obj):
-        return obj.cantidad * obj.producto.precio
-    
-    readonly_fields = ('get_subtotal',)
-    
-
-@admin.register(Pedido)
-class PedidoAdmin(admin.ModelAdmin):
-    list_display = ('id', 'codigo', 'usuario', 'total', 'fecha_registro', 'fecha_ult_act')
-    inlines = (ItemPedidoTabInline,)
