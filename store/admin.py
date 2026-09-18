ab2aec771b223565aabee8f64400e8691a3e630b
eldudyaguirre <143123379+eldudyaguirre@users.noreply.github.com>
2026-09-18 17:50:11 -0500

    Limpiar administración de modelos ecommerce

--- store/admin.py
+++ store/admin.py
@@ -1,12 +1,6 @@
 from django.contrib import admin
-from store.models import Categoria, Producto
 
-admin.site.site_header = 'Administrador de TotalCountsWeb'
 
-@admin.register(Categoria)
-class CategoriaAdmin(admin.ModelAdmin):
-    list_display = ('id', 'nombre', 'slug', 'fecha_registro', 'fecha_ult_act')
-    
-@admin.register(Producto)
-class ProductoAdmin(admin.ModelAdmin):
-    list_display = ('id', 'nombre', 'descripcion', 'precio', 'activo', 'fecha_registro', 'fecha_ult_act')
+admin.site.site_header = 'Administración TotalCounts'
+admin.site.site_title = 'TotalCounts'
+admin.site.index_title = 'Panel de administración'
