e9fc49a140a23c955672024328e6ee43cac46dce
eldudyaguirre <143123379+eldudyaguirre@users.noreply.github.com>
2026-09-18 17:50:08 -0500

    Simplificar rutas del portal

--- store/urls.py
+++ store/urls.py
@@ -1,15 +1,11 @@
 from django.urls import path
+
 from . import views
+
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('signin/', views.do_signin, name='signin'),
     path('logout/', views.do_logout, name='logout'),
-    path('signup/', views.do_signup, name='signup'),
-    path('success-signup/', views.success_signup, name='success_signup'),
     path('about/', views.about, name='about'),
-    #path('sign-in/', views.signinnow, name='sign-in'),
-    #path('sign-up/', views.signupnow, name='sign-up'),
-
 ]
-
