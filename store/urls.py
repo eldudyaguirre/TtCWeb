4e322782eaa2de57d9f8206b0bd3d1017511ab37
eldudyaguirre <143123379+eldudyaguirre@users.noreply.github.com>
2026-09-20 10:44:45 -0500

    Add Contactanos route

--- store/urls.py
+++ store/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('signin/', views.do_signin, name='signin'),
     path('logout/', views.do_logout, name='logout'),
     path('about/', views.about, name='about'),
+    path('contactanos/', views.contactanos, name='contactanos'),
     path('plantillas/', views.template_catalog, name='template_catalog'),
     path('plantillas/<slug:slug>/', views.template_preview, name='template_preview'),
 ]
