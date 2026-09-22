48fff570e19c539501eaafec6e1a969b658039f6
eldudyaguirre <143123379+eldudyaguirre@users.noreply.github.com>
2026-09-22 15:29:24 -0500

    Registrar modelos del módulo Parámetros

--- store/models/__init__.py
+++ store/models/__init__.py
@@ -1,4 +1,5 @@
 """Modelos del portal de clientes TotalCounts."""
 
 from .cliente import Cliente
+from .parametros import Establecimiento, ParametrosCliente, PuntoEmision, SecuencialDocumento
 from .usuario_cliente import UsuarioCliente
