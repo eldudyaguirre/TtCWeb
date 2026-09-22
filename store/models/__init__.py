1390ee8544b12ad2a0571ccbdc18ee53aefe642f
eldudyaguirre <143123379+eldudyaguirre@users.noreply.github.com>
2026-09-22 15:59:30 -0500

    Implementar almacenamiento central de archivos por RUC

--- store/models/__init__.py
+++ store/models/__init__.py
@@ -1,5 +1,6 @@
 """Modelos del portal de clientes TotalCounts."""
 
+from .archivo import Archivo
 from .cliente import Cliente
 from .parametros import Establecimiento, ParametrosCliente, PuntoEmision, SecuencialDocumento
 from .usuario_cliente import UsuarioCliente
