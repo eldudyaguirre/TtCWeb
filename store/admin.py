f980761a59be9d9ff1a84234e5cb12a59d1b0e00
eldudyaguirre <143123379+eldudyaguirre@users.noreply.github.com>
2026-09-22 15:31:30 -0500

    Registrar modelos de Parámetros en administración

--- store/admin.py
+++ store/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 
-from .models import Cliente, UsuarioCliente
+from .models import (
+    Cliente,
+    Establecimiento,
+    ParametrosCliente,
+    PuntoEmision,
+    SecuencialDocumento,
+    UsuarioCliente,
+)
 
 
 admin.site.site_header = 'Administración TotalCounts'
@@ -21,3 +28,31 @@ class UsuarioClienteAdmin(admin.ModelAdmin):
     list_display = ('usuario', 'cliente', 'rol', 'activo', 'creado_en')
     search_fields = ('usuario__username', 'cliente__ruccedcli', 'cliente__nomclient')
     list_filter = ('rol', 'activo')
+
+
+
+@admin.register(ParametrosCliente)
+class ParametrosClienteAdmin(admin.ModelAdmin):
+    list_display = ('cliente', 'ambiente', 'certificado_nombre', 'activo', 'actualizado_en')
+    search_fields = ('cliente__ruccedcli', 'cliente__nomclient')
+    list_filter = ('ambiente', 'activo')
+
+
+@admin.register(Establecimiento)
+class EstablecimientoAdmin(admin.ModelAdmin):
+    list_display = ('cliente', 'codigo', 'nombre', 'direccion', 'activo')
+    search_fields = ('cliente__ruccedcli', 'cliente__nomclient', 'codigo', 'nombre')
+    list_filter = ('activo',)
+
+
+@admin.register(PuntoEmision)
+class PuntoEmisionAdmin(admin.ModelAdmin):
+    list_display = ('establecimiento', 'codigo', 'nombre', 'activo')
+    search_fields = ('establecimiento__cliente__ruccedcli', 'establecimiento__cliente__nomclient', 'codigo', 'nombre')
+    list_filter = ('activo',)
+
+
+@admin.register(SecuencialDocumento)
+class SecuencialDocumentoAdmin(admin.ModelAdmin):
+    list_display = ('punto_emision', 'tipo_documento', 'secuencial_actual', 'activo')
+    list_filter = ('tipo_documento', 'activo')
