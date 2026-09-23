24ac2fe586b647e148ff0a35675922624affad88
eldudyaguirre <143123379+eldudyaguirre@users.noreply.github.com>
2026-09-23 14:18:52 -0500

    Agregar rutas de retencion y liquidacion de compra

--- store/urls.py
+++ store/urls.py
@@ -35,6 +35,8 @@ urlpatterns = [
     path('portal/emitir/factura/', views.factura_emitir, name='factura_emitir'),
     path('portal/emitir/nota-credito/', views.nota_credito_emitir, name='nota_credito_emitir'),
     path('portal/emitir/guia-remision/', views.guia_remision_emitir, name='guia_remision_emitir'),
+    path('portal/emitir/retencion/', views.retencion_emitir, name='retencion_emitir'),
+    path('portal/emitir/liquidacion-compra/', views.liquidacion_compra_emitir, name='liquidacion_compra_emitir'),
 
     path('portal/emitir/nota-credito/buscar-sustento/', views.nota_credito_buscar_sustento, name='nota_credito_buscar_sustento'),
 
