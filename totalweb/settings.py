1148820b2cf6d7b0609efd40a85f95b5bc62c908
eldudyaguirre <143123379+eldudyaguirre@users.noreply.github.com>
2026-09-24 10:32:10 -0500

    Priorizar plantillas de TotalCounts sobre Django admin

--- totalweb/settings.py
+++ totalweb/settings.py
@@ -42,13 +42,13 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
+    'store.apps.StoreConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'store.apps.StoreConfig',
     'django_extensions',
     'django_bootstrap5',
     'totalweb',
